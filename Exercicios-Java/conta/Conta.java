27f26cd487bb825a989c708c83a87882bcbc01b3
William Araujo <will.araujo777@gmail.com>
2021-07-03 01:16:03 -0300

    Finalizado

--- Exercicios-Java/conta/Conta.java
+++ Exercicios-Java/conta/Conta.java
@@ -1,51 +1,58 @@
 
-import java.text.DecimalFormat;
-import java.util.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Conta {
-    
+
     private String nomeTitular;
-    private int    numero;
-    private int    agencia;
+    private int numero;
+    private int agencia;
     private double saldo;
-    private Date   data_abertura;
+    private Date data_abertura;
 
     public Conta() {
         super();
     }
 
-    public Conta(String nomeTitular, int numero, int agencia, double saldo, Date data_abertura) {
+    public Conta(String nomeTitular, int numero, int agencia, double saldo, String data_abertura) {
         this.nomeTitular = nomeTitular;
         this.numero = numero;
         this.agencia = agencia;
         this.saldo = saldo;
-        this.data_abertura = data_abertura;
+        this.data_abertura = converteData(data_abertura);
     }
 
-    public double sacar(double valor){
+    public Date converteData(String data) {
+        SimpleDateFormat formatador = new SimpleDateFormat("dd/MM/yyyy");
+        Date dataFormatada = null;
+        try {
+            dataFormatada = formatador.parse(data);
+        } catch (ParseException e) {
+            System.err.print("Erro ao formatar");
+        }
+
+        return dataFormatada;
+    }
+
+    public double sacar(double valor) {
         this.saldo -= valor;
         return valor;
     }
 
-    public double depositar(double valor){
+    public double depositar(double valor) {
         this.saldo += valor;
         return valor;
     }
 
-    public void calculaRendimento(int meses){
-        DecimalFormat formatador = new DecimalFormat("0.00");
-        
-        double valorRendido = 500;
+    public double calculaRendimento(int meses) {
+        double valorRendido = this.saldo;
         double percentual = 0;
-        for(int cont = 0; cont < meses; cont++){
-           percentual = valorRendido * (0.5/100);
-           valorRendido = valorRendido + percentual;
+        for (int cont = 0; cont < meses; cont++) {
+            percentual = valorRendido * (0.5 / 100);
+            valorRendido = valorRendido + percentual;
         }
-
-        double acrescimo = valorRendido - 500;
-        System.out.println("Sua conta em " + meses + " meses vai render de R$ " + formatador.format(acrescimo) + " reais" +
-                           "\nFicaria um total de R$ " + formatador.format(valorRendido) + " reais");
-
+        return valorRendido;
     }
 
     public String getNomeTitular() {
--- Exercicios-Java/conta/TestaConta.java
+++ Exercicios-Java/conta/TestaConta.java
@@ -1,8 +1,14 @@
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+
 import javax.swing.JOptionPane;
+
 public class TestaConta {
 
     public static void main(String[] args) {
-        Conta conta = new Conta();
+
+        SimpleDateFormat formatadorData = new SimpleDateFormat("dd/MM/yyyy");
+        DecimalFormat formatadorValor = new DecimalFormat("0.00");
 
         int agencia = 0;
         boolean continua = true;
@@ -10,96 +16,180 @@ public class TestaConta {
         int tentativa = 3;
         int opcao = 0;
 
-        conta.setAgencia(1234);
-        conta.setNumero(777);
-        conta.setSaldo(170);
-        conta.setNomeTitular("William");
+        Conta conta = new Conta("William Araujo", 777, 1234, 700.0, "17/07/2015");
 
-        msgDefault("Boas vindas ao banco!");
+        msgDefault("Boas vindas ao sistema do banco Will SA.!", "OLÁ!");
 
-        //Laço de repetição responsável por verificar se a agência está correta
+        // Laço de repetição responsável por verificar se a agência está correta
         while (continua) {
-            agencia = msgInput("Digite sua agencia: ");
-            if (conta.getAgencia() == agencia) {
-                msgDefault("Agência correta! Prossiga");
-                continua = false;
-            } else {
-                tentativa -= 1;
-                if (tentativa < 1) {
-                    msgErro("Conta bloqueada, não há mais tentativas restantes, contate o ADM!");
-                    System.exit(0);
+            // Esse try está ai para capturar caso o usuario digite uma letra ao invéz de um
+            // numero
+            try {
+                agencia = msgInput("Digite sua agencia: ", "AGÊNCIA");
+                if (conta.getAgencia() == agencia) {
+                    msgDefault("Agência encontrada! Prossiga apertando em OK", "AGÊNCIA");
+                    continua = false;
                 } else {
-                    msgErro("Agencia incorreta! Digite novamente! " + "\nPS: Você tem " + tentativa
-                            + " tentativa(s) restante(s)!!");
+                    tentativa -= 1;
+                    if (tentativa < 1) {
+                        msgErro("Conta bloqueada, não há mais tentativas restantes, contate o ADM!", "ERRO");
+                        System.exit(0);
+                    } else {
+                        msgErro("Agencia incorreta! Digite novamente! " + "\nPS: Você tem " + tentativa
+                                + " tentativa(s) restante(s)!", "ERRO");
+                    }
                 }
-            }
-        }
 
+            } catch (NumberFormatException e) {                 // Este catch lança um Exception caso o usuario tenha coloca uma letra ao invéz
+                msgErro("Por favor, apenas numeros!", "ERRO");  // de um numero,
+            }                                                   // gerando uma mensagem e retornando para a opção de digitar a agência
+        }
         continua = true;
         tentativa = 3;
 
         // Laço de repetição responsável por verificar se a conta está correta
         while (continua) {
-            numeroConta = msgInput("Digite sua conta: ");
-            System.out.println(numeroConta);
-            if (conta.getNumero() == numeroConta) {
-                msgDefault("Numero da conta correto! Prossiga para os nossos serviços ^^");
-                continua = false;
-            } else {
-                tentativa -= 1;
-                if (tentativa < 1) {
-                    msgErro("Conta bloqueada, não há mais tentativas restantes, contate o ADM!");
-                    System.exit(0);
+            // Esse try está ai para capturar caso o usuario digite uma letra ao invéz de um
+            // numero
+            try {
+                numeroConta = msgInput("Digite sua conta: ", "CONTA");
+                System.out.println(numeroConta);
+                if (conta.getNumero() == numeroConta) {
+                    msgDefault("Numero da conta encontrado! Prossiga para os nossos serviços", "CONTA");
+                    continua = false;
                 } else {
-                    msgErro("Numero da conta incorreto! Digite novamente! " + "\nPS: Você tem " + tentativa
-                            + " tentativa(s) restante(s)!!");
+                    tentativa -= 1;
+                    if (tentativa < 1) {
+                        msgErro("Conta bloqueada, não há mais tentativas restantes, contate o ADM!", "ERRO");
+                        System.exit(0);
+                    } else {
+                        msgErro("Numero da conta incorreto! Digite novamente! " + "\nPS: Você tem " + tentativa
+                                + " tentativa(s) restante(s)!", "ERRO");
+                    }
                 }
-            }
+
+               
+              
+               
+            } catch (NumberFormatException e) {                 // Este catch lança um Exception caso o usuario tenha coloca uma letra ao invéz
+                msgErro("Por favor, apenas numeros!", "ERRO");  // de um numero,
+            }                                                   // gerando uma mensagem e retornando para a opção de digitar a conta
         }
-
         continua = true;
+
+        // Laço de repetição responsável pelo menu do programa, onde ele testa todas as
+        // opções e também finaliza o programa
         while (continua) {
-            opcao = msgInput("Selecione a opção desejada: " + "\n 1 = Ver Saldo" + "\n 2 = Depositar" + "\n 3 = Sacar"
-                    + "\n 4 = Verificar rendimento da conta " + "\n 5 = Finalizar sistema ");
 
-            switch (opcao) {
-                case 1:
-                    msgDefault("O seu saldo é de R$ " + conta.getSaldo() + ",00");
-                    break;
+            // a variavel "opcao" está sendo responsável por capturar a opção escolhida pelo
+            // usuario
+            opcao = msgInput("Selecione a opção desejada: " + "\n 1 = Dados da conta" + "\n 2 = Depositar"
+                    + "\n 3 = Sacar" + "\n 4 = Verificar rendimento da conta " + "\n 5 = Finalizar sistema ",
+                    "MENU PRINCIPAL");
 
-                case 2:
-                    msgDefault("O seu saldo é de R$ " + conta.getSaldo() + ",00");
-                    break;
+            // Esse try está ai para capturar caso o usuario digite uma letra ao invéz de um
+            // numero
+            try {
 
-                case 3:
-                    msgDefault("O seu saldo é de R$ " + conta.getSaldo() + ",00");
-                    break;
-                case 4:
-                    msgDefault("O seu saldo é de R$ " + conta.getSaldo() + ",00");
-                    break;
-                case 5:
-                    msgDefault("Obrigado por utilizar nossos serviços " + conta.getNomeTitular() + "\nO banco será finalizado!");
-                    System.exit(0);
-                    break;
+                // aqui está sendo aberto um switch da opção escolhida pelo usuario
+                // anteriormente, cada opção equivale a uma funcionalidade
+                switch (opcao) {
+
+                    // primeira opção reponsável apenas por apresentar os dados da conta (Titular,
+                    // Saldo e data de abertura)
+                    case 1:
+                        msgDefault(
+                                "Nome do titular: " + conta.getNomeTitular() + "\nSaldo: R$ " + conta.getSaldo()
+                                        + "\nData de abertura: " + formatadorData.format(conta.getData_abertura()),
+                                "DADOS DA CONTA");
+                        break;
+
+                    //segunda opção responsável por realizar um depósito na conta do usuario, a mesma verifica se ele é maior que 0 para continuar
+                    case 2:
+                        double deposito = msgInput("Saldo atual: " + conta.getSaldo()
+                                + "\nInsira a quantia que deseja depositar na conta:", "DEPOSITO");
+                        if (deposito > 0) {
+                            conta.setSaldo(conta.getSaldo() + deposito);
+                            msgDefault("O deposito no valor de: " + deposito + " foi realizado com sucesso!"
+                                    + "\nNovo saldo é de " + conta.getSaldo(), "DEPOSITO");
+                        } else {
+                            msgErro("Erro! Por favor, insira um valor válido!", "ERRO");
+                        }
+                        break;
                     
-                default:
-                    msgErro("Opção inválida! Por favor, selecione uma opção entre 1 e 5!");
-            }
+                    //terceira opção responsável por realizar um saque na conta do usuario, a mesma verifica se ele é maior que 0 para continuar
+                    //também verifica se é menor que o saldo atual
+                    case 3:
+                        double saque = msgInput(
+                                "Saldo atual: " + conta.getSaldo() + "\nInsira a quantia que deseja sacar da conta:",
+                                "SAQUE");
+
+                        if (saque > 0 && saque <= conta.getSaldo()) {
+                            conta.setSaldo(conta.getSaldo() - saque);
+                            msgDefault("O saque no valor de: " + saque + " foi realizado com sucesso!"
+                                    + "\nNovo saldo é de " + conta.getSaldo(), "SAQUE");
+                        } else if (conta.getSaldo() == 0) {
+                            msgErro("Erro! Sua conta esta zerada, impossivel realizar o saque!", "ERRO");
+                        } else if (saque > conta.getSaldo()) {
+                            msgErro("Erro! você não possui este valor em conta, impossivel realizar o saque!", "ERRO");
+                        } else {
+                            msgErro("Erro! Por favor, insira um valor válido!", "ERRO");
+                        }
+                        break;
+
+                    //quarta opção é reponsável por realizar um teste de rendimento na conta, o usuario insere uma quantidade de meses que deseja
+                    //deixar o dinheiro rendendo, e o programa retorna a quantia que o saldo teria depois daquele período
+                    case 4:
+                        int meses = msgInput("O seu dinheiro está rendendo 0.5% ao mês no nosso banco!"
+                                + "\n Deseja realizar um teste e verificar quanto ele irá render em X meses?"
+                                + "\n Insira a quantidade de meses que deseja deixar rendendo: ", "RENDIMENTO");
+
+                        double valorRendido = conta.calculaRendimento(meses);
+                        double acrescimo = valorRendido - conta.getSaldo();
+
+                        msgDefault("Sua conta em " + meses + " meses vai render R$ " + formatadorValor.format(acrescimo)
+                                + " reais" + "\nFicaria um total de R$ " + formatadorValor.format(valorRendido)
+                                + " reais", "RENDIMENTO");
+
+                        break;
+                    
+                    //quinta opção, responsável por finalizar o sistema, ela pergunta se o usuario realmente deseja fazer tal ato
+                    case 5:
+                        int i = msgConfirmar("Você realmente deseja finalizar o sistema?", "ATENÇÃO");
+                        if (i == JOptionPane.YES_OPTION) {
+                            msgDefault("Obrigado por utilizar nossos serviços!" + "\nO sistema será finalizado."
+                                    + "\nAté mais " + conta.getNomeTitular(), "SAIR");
+                            System.exit(0);
+                        } else {
+                            break;
+                        }
+                    
+                    //essa opção é para caso o usuario selecione uma opção que não condiz com as do menu, o que gera um erro e retorna ao menu
+                    default:
+                        msgErro("Opção inválida! Por favor, selecione uma opção entre 1 e 5!", "ERRO");
+                }
+            } catch (NumberFormatException e) {                     // Este catch lança um Exception caso o usuario tenha coloca uma letra ao invés
+                msgErro("Por favor, apenas numeros!", "ERRO");      // de um numero,
+            }                                                       // gerando uma mensagem e retornando para a opção de escolher uma opção
+
         }
     }
 
     // Funções que criei para exibir as mensagem do JOptionPane, para evitar digitar
     // o JOption o tempo todo!
-    public static void msgDefault(String msg) {
-        JOptionPane.showMessageDialog(null, msg, "Banco WILL SA.", JOptionPane.INFORMATION_MESSAGE);
+    public static void msgDefault(String msg, String titulo) {
+        JOptionPane.showMessageDialog(null, msg, titulo, JOptionPane.INFORMATION_MESSAGE);
     }
 
-    public static int msgInput(String opcao) {
-        return Integer.parseInt(JOptionPane.showInputDialog(opcao));
+    public static int msgInput(String msg, String titulo) {
+        return Integer.parseInt(JOptionPane.showInputDialog(null, msg, titulo, JOptionPane.QUESTION_MESSAGE));
     }
 
-    public static void msgErro(String msg) {
-        JOptionPane.showMessageDialog(null, msg, "Banco WILL SA.", JOptionPane.ERROR_MESSAGE);
+    public static int msgConfirmar(String msg, String titulo) {
+        return JOptionPane.showConfirmDialog(null, msg, titulo, JOptionPane.YES_NO_OPTION);
     }
 
+    public static void msgErro(String msg, String titulo) {
+        JOptionPane.showMessageDialog(null, msg, titulo, JOptionPane.ERROR_MESSAGE);
+    }
 }
